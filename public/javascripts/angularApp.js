99e7bd89e31b35d373230954e7571a5e9a8369cb
Spencer Liechty <sliechty@Spencers-MacBook-Air.local>
2015-07-02 14:02:12 -0700

    Testing something

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -204,6 +204,7 @@ function($scope, $state, auth){
 	$scope.user = {};
 
 	$scope.register = function() {
+
 		auth.register($scope.user).error(function(error){
 			$scope.error = error;
 		}).then(function() {
